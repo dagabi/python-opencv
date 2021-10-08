281eab2846311fda34b995ca03847facdaa6bc1a
GABRIELPC\Gabriel <dagabi@gmail.com>
2021-10-09 00:09:22 +0300

    add resize

--- bookCode/6-1-translation.py
+++ bookCode/6-1-translation.py
@@ -1,6 +1,5 @@
 import numpy as np
 import argparse
-import imutils
 import cv2
 
 def translate(image, x, y):
@@ -8,11 +7,30 @@ def translate(image, x, y):
     shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))    
     return shifted
 
-def rotate(image, degrees):
+def rotate(image, angle, center = None, scale = 1.0):
     (h, w) = image.shape[:2]
-    center = (w // 2, h //2)
-    M = cv2.getRotationMatrix2D(center, degrees, 1.0)
+    if center is None:
+        center = (w // 2, h //2)
+
+    M = cv2.getRotationMatrix2D(center, angle, scale)
     return cv2.warpAffine(image, M, (w,h))
+
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h,w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    
+    if width is None:
+        r = height / float(h)
+        dim = (int(w*r), height)
+    
+    else:
+        r = width / float(w)
+        dim = (width, int(h*r))
+
+    return cv2.resize(image, dim, interpolation=inter)
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required= True, help="path to image")
